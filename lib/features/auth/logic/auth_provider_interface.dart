46d6d99d9a3dfc744e2c1a9e04774153e9e8c63b
Gh0sTed <113895444+ProfessorMB21@users.noreply.github.com>
2026-03-25 23:59:49 +0300

    implemented auth provider interface;

---
+++ lib/features/auth/logic/auth_provider_interface.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+
+abstract class AuthProviderInterface {
+  Future<void> initialize();
+  Future<void> signIn(String email, String password);
+  Future<void> signUp(String email, String password, String fullName);
+  Future<void> signOut();
+  Stream<AuthState> get authStateChanges;
+  String? get currentUserId;
+  String? get currentUserEmail;
+  bool get isAuthenticated;
+}
+
+class AuthState {
+  final String? userId;
+  final String? email;
+  final bool isAuthenticated;
+
+  AuthState({this.userId, this.email, required this.isAuthenticated});
+}
